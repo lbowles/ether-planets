2f092cab6b3ba9f3b6cc5d02cc8ddb6437f9b28e
lbowles <lspbowles@gmail.com>
2023-04-09 21:53:45 +0200

    center the iframe full screen

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,16 @@
 import React from "react"
 import logo from "./logo.svg"
 import "./App.css"
-import P5Sketch from "./components/P5Sketch"
 
 const htmlFileURL = process.env.PUBLIC_URL + "/landing.html"
 
 function App() {
   return (
     <div className="App">
-      <p style={{ color: "white" }}>sadas</p>
       <iframe
         title="Embedded HTML File"
         src={htmlFileURL}
-        width="100%"
-        height="500px"
+        className="full-screen-iframe"
         style={{ border: "none" }}
       ></iframe>
     </div>
